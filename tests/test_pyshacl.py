8f853ab88d3cc197c2380d73e773ff7b21d08e11
Thomas Wilmering <thomas.wilmering@eccenca.com>
2023-01-18 14:25:59 +0000

    downgrade charset-normalizer to 2.1.1, moved pylint rule changes to functions, use pytest tmp_path

--- tests/test_pyshacl.py
+++ tests/test_pyshacl.py
@@ -5,11 +5,11 @@ from cmem.cmempy.dp.proxy.graph import post, get, delete
 from uuid import uuid4
 from rdflib import Graph, URIRef, RDF
 import os
-
 import pyshacl
+import shutil
 
 
-def post_shacl_shacl(shacl_graph_uri):
+def post_shacl_shacl(shacl_graph_uri, tmp_path):
     shacl_file = os.path.join(pyshacl.__path__[0], "assets", "shacl-shacl.ttl")
     g = Graph()
     g.parse(shacl_file, format="turtle")
@@ -20,15 +20,15 @@ def post_shacl_shacl(shacl_graph_uri):
             URIRef("https://vocab.eccenca.com/shui/ShapeCatalog")
         )
     )
-    temp_file = f"{uuid4()}.nt"
+    temp_file = tmp_path / f"{uuid4()}.nt"
     g.serialize(temp_file, format="nt", encoding="utf-8")
     res = post(shacl_graph_uri, temp_file, replace=True)
-    os.remove(temp_file)
+    shutil.rmtree(tmp_path)
     if res.status_code != 204:
         raise ValueError(f"Response {res.status_code}")
 
 
-def test_execution():
+def test_execution(tmp_path):
     """Test plugin execution"""
     shacl_graph_uri = f"https://example.org/pyshacl-plugin-test/{uuid4()}"
     data_graph_uri = "https://vocab.eccenca.com/shacl/"
@@ -42,7 +42,7 @@ def test_execution():
     include_graphs_labels = True
     add_shui_conforms_to_validation_graph = True
     meta_shacl = False
-    post_shacl_shacl(shacl_graph_uri)
+    post_shacl_shacl(shacl_graph_uri, tmp_path)
     plugin = ShaclValidation(
         data_graph_uri=data_graph_uri,
         shacl_graph_uri=shacl_graph_uri,
