5c1348568791fa7ba9030a7a603e6640cd48cb87
Louis Westerheide <louis.westerheide@eccenca.com>
2026-01-27 11:12:49 +0100

    close input port

--- cmem_plugin_pyshacl/plugin_pyshacl.py
+++ cmem_plugin_pyshacl/plugin_pyshacl.py
@@ -21,6 +21,7 @@ from cmem_plugin_base.dataintegration.parameter.graph import (
     get_graphs_list,
 )
 from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
+from cmem_plugin_base.dataintegration.ports import FixedNumberOfInputs
 from cmem_plugin_base.dataintegration.types import (
     BoolParameterType,
     IntParameterType,
@@ -351,6 +352,7 @@ class ShaclValidation(WorkflowPlugin):
         self.remove_thesaurus_graph_type = remove_thesaurus_graph_type
         self.remove_shape_catalog_graph_type = remove_shape_catalog_graph_type
         self.max_validation_depth = max_validation_depth
+        self.input_ports = FixedNumberOfInputs([])
 
     def add_prov(self, validation_graph: Graph, utctime: str) -> Graph:
         """Add provenance data"""
